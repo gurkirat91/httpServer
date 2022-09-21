eb4ca0d9ea8eb8696d396137378ff44963180fe9
Gurkirat Singh <gurkiratsingh@Gurkirats-MacBook-Air.local>
2022-09-21 19:28:44 +0530

    http

---
+++ server.c
@@ -0,0 +1,130 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <netdb.h> // for getnameinfo()
+
+// Usual socket headers
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+#include <arpa/inet.h>
+
+//#define SIZE 1024
+#define BACKLOG 10  // Passed to listen()
+//........................getting data from config.txt..................................//
+int port;  // for getting port no 
+char html[100];   //for getting the address of html file
+//.....................................................................................//
+void report(struct sockaddr_in *serverAddress);
+void getHTML(){
+    FILE *fp;
+    fp=fopen("config.txt", "r");
+
+    //char c[100];
+    char holder;
+    int line=0;
+    while((holder=fgetc(fp)) != EOF) {
+        if(holder == '\n') line++;
+        if(line == 1) break; /* 1 because count start from 0,you know */
+    }
+    fscanf(fp, "%[^\n]",html);
+    //printf("%s", c);
+}
+void getPortNo(){
+	char c[10];
+	FILE *fptr;
+	fptr = fopen("config.txt", "r");
+	fscanf(fptr, "%[^\n]",c);
+	port=atoi(c);
+        //printf("%s", c);
+        fclose(fptr);
+}
+void setHttpHeader(char httpHeader[])
+{
+    // File object to return
+    getHTML();
+    FILE *htmlData = fopen(html, "r"); 
+   // FILE *htmlData = fopen("/home/kirat/Desktop/http/index.html", "r");
+
+    char line[100];
+    char responseData[8000];
+    while (fgets(line, 100, htmlData) != 0) {
+        strcat(responseData, line);
+    }
+    // char httpHeader[8000] = "HTTP/1.1 200 OK\r\n\n";
+    strcat(httpHeader, responseData);
+}
+
+int main(void)
+{
+    char httpHeader[8000] = "HTTP/1.1 200 OK\r\n\n";
+
+    // Socket setup: creates an endpoint for communication, returns a descriptor
+    // -----------------------------------------------------------------------------------------------------------------
+    int serverSocket = socket(
+        AF_INET,      // Domain: specifies protocol family
+        SOCK_STREAM,  // Type: specifies communication semantics
+        0             // Protocol: 0 because there is a single protocol for the specified family
+    );
+
+    // Construct local address structure
+    // -----------------------------------------------------------------------------------------------------------------
+    getPortNo();
+    struct sockaddr_in serverAddress;
+    serverAddress.sin_family = AF_INET;
+    serverAddress.sin_port = htons(port);
+    serverAddress.sin_addr.s_addr = htonl(INADDR_LOOPBACK);//inet_addr("127.0.0.1");
+
+    // Bind socket to local address
+    // -----------------------------------------------------------------------------------------------------------------
+    // bind() assigns the address specified by serverAddress to the socket
+    // referred to by the file descriptor serverSocket.
+    bind(
+        serverSocket,                         // file descriptor referring to a socket
+        (struct sockaddr *) &serverAddress,   // Address to be assigned to the socket
+        sizeof(serverAddress)                 // Size (bytes) of the address structure
+    );
+
+    // Mark socket to listen for incoming connections
+    // -----------------------------------------------------------------------------------------------------------------
+    int listening = listen(serverSocket, BACKLOG);
+    if (listening < 0) {
+        printf("Error: The server is not listening.\n");
+        return 1;
+    }
+    report(&serverAddress);     // Custom report function
+    setHttpHeader(httpHeader);  // Custom function to set header
+    int clientSocket;
+
+    // Wait for a connection, create a connected socket if a connection is pending
+    // -----------------------------------------------------------------------------------------------------------------
+    while(1) {
+        clientSocket = accept(serverSocket, NULL, NULL);
+        send(clientSocket, httpHeader, sizeof(httpHeader), 0);
+        close(clientSocket);
+    }
+    return 0;
+}
+
+void report(struct sockaddr_in *serverAddress)
+{
+    char hostBuffer[INET6_ADDRSTRLEN];
+    char serviceBuffer[NI_MAXSERV]; // defined in `<netdb.h>`
+    socklen_t addr_len = sizeof(*serverAddress);
+    int err = getnameinfo(
+        (struct sockaddr *) serverAddress,
+        addr_len,
+        hostBuffer,
+        sizeof(hostBuffer),
+        serviceBuffer,
+        sizeof(serviceBuffer),
+        NI_NUMERICHOST
+    );
+    if (err != 0) {
+        printf("It's not working!!\n");
+    }
+    printf("\n\n\tServer listening on http://%s:%s\n", hostBuffer, serviceBuffer);
+}
